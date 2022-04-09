827cb68045a32ff4f65b0519459964911bca4d6a
Philipp Muens <raute1337@gmx.de>
2022-04-09 20:32:58 +0200

    Add `Unions` section code

---
+++ chapter-1/unions.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+const expectEqual = std.testing.expectEqual;
+
+const Result = union {
+    int: i64,
+    float: f64,
+    bool: bool,
+};
+
+test "simple union" {
+    var result = Result{ .int = 1234 };
+    result.float = 12.32;
+}
+
+const Tag = enum { a, b, c };
+const Tagged = union(Tag) { a: u8, b: f32, c: bool };
+
+test "switch on tagged union" {
+    var value = Tagged{ .b = 1.5 };
+    switch (value) {
+        .a => |*byte| byte.* += 1,
+        .b => |*float| float.* *= 2,
+        .c => |*b| b.* = !b.*,
+    }
+
+    try expectEqual(value.b, 3);
+}
