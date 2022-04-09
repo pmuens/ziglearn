0c0b608ec0ab1b529ebaaecbe987a24e8b766302
Philipp Muens <raute1337@gmx.de>
2022-04-09 18:40:00 +0200

    Add `Runtime Safety` section code

---
+++ chapter-1/runtime_safety.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+const expectEqual = std.testing.expectEqual;
+
+test "out of bounds" {
+    const a = [3]u8{ 1, 2, 3 };
+
+    var index: u8 = 5;
+    const b = a[index];
+
+    _ = b;
+}
+
+test "out of bounds, no safety" {
+    @setRuntimeSafety(false);
+
+    const a = [3]u8{ 1, 2, 3 };
+
+    var index: u8 = 5;
+    const b = a[index];
+
+    _ = b;
+}
