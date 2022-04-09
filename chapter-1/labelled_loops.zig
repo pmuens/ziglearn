89878a7ac0ba4ed96ecaf3dbc73e499f85e6caef
Philipp Muens <raute1337@gmx.de>
2022-04-09 20:51:07 +0200

    Add `Labelled Loops` section code

---
+++ chapter-1/labelled_loops.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+const expectEqual = std.testing.expectEqual;
+
+test "nested continue" {
+    var count: usize = 0;
+    outer: for ([_]i32{ 1, 2, 3, 4, 5, 6, 7, 8 }) |_| {
+        for ([_]i32{ 1, 2, 3, 4, 5 }) |_| {
+            count += 1;
+            continue :outer;
+        }
+    }
+
+    try expectEqual(count, 8);
+}
