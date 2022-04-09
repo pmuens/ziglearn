559e37446e620be7975f505c7087b4d2ae0f424d
Philipp Muens <raute1337@gmx.de>
2022-04-09 17:28:44 +0200

    Add `For` section code

---
+++ chapter-1/for.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+const expectEqual = std.testing.expectEqual;
+
+test "for" {
+    const string = [_]u8{ 'a', 'b', 'c' };
+
+    for (string) |character, index| {
+        _ = character;
+        _ = index;
+    }
+
+    for (string) |character| {
+        _ = character;
+    }
+
+    for (string) |_, index| {
+        _ = index;
+    }
+
+    for (string) |_| {}
+}
