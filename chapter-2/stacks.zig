b3003bd5d566e986bea1100bff1331b0ff4e538b
Philipp Muens <raute1337@gmx.de>
2022-04-10 20:07:03 +0200

    Add `Stacks` section code

---
+++ chapter-2/stacks.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+const eql = std.meta.eql;
+const ArrayList = std.ArrayList;
+const expect = std.testing.expect;
+const test_allocator = std.testing.allocator;
+
+test "stack" {
+    const string = "(()())";
+    var stack = ArrayList(usize).init(test_allocator);
+    defer stack.deinit();
+
+    const Pair = struct { open: usize, close: usize };
+    var pairs = ArrayList(Pair).init(test_allocator);
+    defer pairs.deinit();
+
+    for (string) |char, i| {
+        if (char == '(') try stack.append(i);
+        if (char == ')')
+            try pairs.append(.{
+                .open = stack.pop(),
+                .close = i,
+            });
+    }
+
+    for (pairs.items) |pair, i| {
+        try expect(eql(pair, switch (i) {
+            0 => Pair{ .open = 1, .close = 2 },
+            1 => Pair{ .open = 3, .close = 4 },
+            2 => Pair{ .open = 0, .close = 5 },
+            else => unreachable,
+        }));
+    }
+}
