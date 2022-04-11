b7c31c039a830b92fee22d0b5473777c9beeaabf
Philipp Muens <raute1337@gmx.de>
2022-04-11 09:07:12 +0200

    Add `Packed Structs` section code

---
+++ chapter-4/packed_structs.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+const expect = std.testing.expect;
+
+const MovementState = packed struct {
+    running: bool,
+    crouching: bool,
+    jumping: bool,
+    in_air: bool,
+};
+
+test "packed struct size" {
+    try expect(@sizeOf(MovementState) == 1);
+    try expect(@bitSizeOf(MovementState) == 4);
+
+    const state = MovementState{
+        .running = true,
+        .crouching = true,
+        .jumping = true,
+        .in_air = true,
+    };
+    _ = state;
+}
